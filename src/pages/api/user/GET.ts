352763d09f2f0e937f08dc20449af984abdb5da5
diamond <2580456922@qq.com>
2024-02-18 23:13:05 +0800

    23:12

--- src/pages/api/user/GET.ts
+++ src/pages/api/user/GET.ts
@@ -3,13 +3,7 @@ import  {useRouter} from 'next/router'
 import {PrismaClient} from '@prisma/client'
 const prisma=new PrismaClient()
 const Where=async (req:NextApiRequest,res:NextApiResponse)=>{
-   
-   
-  //  await prisma.linkCardGather.deleteMany({
-  //        where:{
-  //         open:true
-  //        }
-  //   })  
+ 
      await prisma.linkCard.deleteMany({
 
      })     
